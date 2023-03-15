f0495355198b4507bbf00dfc372262d960e7afda
Saulo Caliman Gomes <saulocalimangomes@gmail.com>
2023-03-15 14:14:01 -0300

    arquivos do projeto

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public int vida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PerderVida(int dano) {
+        vida -= dano;
+        if (vida <= 0) { 
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedRotation;
+    public float speed;
+    Rigidbody rb;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimento();
+        Atacar();
+    }
+
+    void Movimento() {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(hAxis, 0, vAxis);
+
+        rb.velocity = move.normalized * speed * Time.deltaTime;
+
+        animator.SetFloat("Velocidade",move.sqrMagnitude);
+
+        if (move != Vector3.zero)
+        {
+            Quaternion rotacao = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards
+                (transform.rotation, rotacao, speedRotation * Time.deltaTime);
+        }
+    }
+
+    void Atacar() {
+        if (Input.GetButtonDown("Fire1")) { 
+            animator.SetTrigger("Atk");
+        }
+    }
+
+}
